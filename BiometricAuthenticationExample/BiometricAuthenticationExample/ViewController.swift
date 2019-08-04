bd16a02cfc1ea007840f8aafa63633032809dced
computervision0907 <m.e.business0522@gmail.com>
2019-08-04 10:06:18 -0500

    final version

---
+++ BiometricAuthenticationExample/BiometricAuthenticationExample/ViewController.swift
@@ -0,0 +1,127 @@
+//
+//  ViewController.swift
+//  BiometricAuthenticationExample
+//
+//  Created by Rushi on 28/10/17.
+//  Copyright © 2018 Rushi Sangani. All rights reserved.
+//
+
+import UIKit
+import BiometricAuthentication
+
+class ViewController: UIViewController {
+
+    // MARK: - Outlets
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func loginButtonClicked(_ sender: Any) {
+        view.endEditing(true)
+        
+        // show success alert
+        showLoginSucessAlert()
+    }
+    
+    @IBAction func biometricAuthenticationClicked(_ sender: Any) {
+        
+        // Set AllowableReuseDuration in seconds to bypass the authentication when user has just unlocked the device with biometric
+        BioMetricAuthenticator.shared.allowableReuseDuration = 30
+        
+        // start authentication
+        BioMetricAuthenticator.authenticateWithBioMetrics(reason: "") { [weak self] (result) in
+                
+            switch result {
+            case .success( _):
+                
+                // authentication successful
+                self?.showLoginSucessAlert()
+                
+            case .failure(let error):
+                
+                switch error {
+                    
+                // device does not support biometric (face id or touch id) authentication
+                case .biometryNotAvailable:
+                    self?.showErrorAlert(message: error.message())
+                    
+                // No biometry enrolled in this device, ask user to register fingerprint or face
+                case .biometryNotEnrolled:
+                    self?.showGotoSettingsAlert(message: error.message())
+                    
+                // show alternatives on fallback button clicked
+                case .fallback:
+                    self?.txtUsername.becomeFirstResponder() // enter username password manually
+                    
+                    // Biometry is locked out now, because there were too many failed attempts.
+                // Need to enter device passcode to unlock.
+                case .biometryLockedout:
+                    self?.showPasscodeAuthentication(message: error.message())
+                    
+                // do nothing on canceled by system or user
+                case .canceledBySystem, .canceledByUser:
+                    break
+                    
+                // show error for any other reason
+                default:
+                    self?.showErrorAlert(message: error.message())
+                }
+            }
+        }
+    }
+    
+    // show passcode authentication
+    func showPasscodeAuthentication(message: String) {
+        
+        BioMetricAuthenticator.authenticateWithPasscode(reason: message) { [weak self] (result) in
+            switch result {
+            case .success( _):
+                self?.showLoginSucessAlert() // passcode authentication success
+            case .failure(let error):
+                print(error.message())
+            }
+        }
+    }
+}
+
+// MARK: - Alerts
+extension ViewController {
+    
+    func showAlert(title: String, message: String) {
+        
+        let okAction = AlertAction(title: OKTitle)
+        let alertController = getAlertViewController(type: .alert, with: title, message: message, actions: [okAction], showCancel: false) { (button) in
+        }
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func showLoginSucessAlert() {
+        showAlert(title: "Success", message: "Login successful")
+    }
+    
+    func showErrorAlert(message: String) {
+        showAlert(title: "Error", message: message)
+    }
+    
+    func showGotoSettingsAlert(message: String) {
+        let settingsAction = AlertAction(title: "Go to settings")
+        
+        let alertController = getAlertViewController(type: .alert, with: "Error", message: message, actions: [settingsAction], showCancel: true, actionHandler: { (buttonText) in
+            if buttonText == CancelTitle { return }
+            
+            // open settings
+            let url = URL(string: UIApplication.openSettingsURLString)
+            if UIApplication.shared.canOpenURL(url!) {
+                UIApplication.shared.open(url!, options: [:])
+            }
+            
+        })
+        present(alertController, animated: true, completion: nil)
+    }
+}
